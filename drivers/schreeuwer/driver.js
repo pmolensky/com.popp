bd75abe1563f4177e7f4228d2226e09d0c4f42bf
mruiter <mruiter@tuxweb.nl>
2016-08-03 13:03:32 +0200

    Finished for the first real test with the device

--- drivers/schreeuwer/driver.js
+++ drivers/schreeuwer/driver.js
@@ -19,7 +19,43 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
       'command_report'			: 'SWITCH_BINARY_REPORT',
       'command_report_parser'		: function( report ){
       return report['Value'] === 'on/enable';
-      }
+      },
+
+
+      'measure_battery': {
+			'command_class'				: 'COMMAND_CLASS_BATTERY',
+			'command_get'				: 'BATTERY_GET',
+			'command_report'			: 'BATTERY_REPORT',
+			'command_report_parser'		: function( report ) {
+				if( report['Battery Level'] === "battery low warning" ) return 1;
+				return report['Battery Level (Raw)'][0];
+			   }
+		  },
+
+
+      'measure_temperature': {
+			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get'				: 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser'		: function(){
+				return {
+					'Sensor Type': 'Temperature (version 1)',
+					'Properties1': {
+						'Scale': 0
+					}
+				}
+			},
+			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
+			'command_report_parser'		: function( report ){
+				if( report['Sensor Type'] !== 'Temperature (version 1)' )
+					return null;
+
+				return report['Sensor Value (Parsed)'];
+			}
+		}
+
+
+
+
     },
     settings: {
                 "siren_trigger_mode": {
