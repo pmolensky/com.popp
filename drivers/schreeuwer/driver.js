4a6baacee4197c78eedb0c5319cc0e8d18f23c56
mruiter <mruiter@tuxweb.nl>
2016-08-15 22:07:30 +0200

    Werken

--- drivers/schreeuwer/driver.js
+++ drivers/schreeuwer/driver.js
@@ -7,57 +7,56 @@ const ZwaveDriver   = require('homey-zwavedriver');
 module.exports = new ZwaveDriver( path.basename(__dirname), {
     debug: true,
     capabilities: {
-      'onoff': {
-      'command_class'			: 'COMMAND_CLASS_SWITCH_BINARY',
-      'command_get'				: 'SWITCH_BINARY_GET',
-      'command_set'				: 'SWITCH_BINARY_SET',
-      'command_set_parser'		: function( value ){
-        return {
-          'Switch Value': value
-          }
-      },
-      'command_report'			: 'SWITCH_BINARY_REPORT',
-      'command_report_parser'		: function( report ){
-      return report['Value'] === 'on/enable';
-        }
-      },
+    		'onoff': {
+    			'command_class'			: 'COMMAND_CLASS_SWITCH_BINARY',
+    			'command_set'				: 'SWITCH_BINARY_SET',
+    			'command_set_parser'		: function( value ){
+    				return {
+    					'Switch Value': value
+    				}
+    			},
+    			'command_report'			: 'SWITCH_BINARY_REPORT',
+    			'command_report_parser'		: function( report ){
+    				return report['Value'] === 'on/enable';
+    			}
+    		},
+        'measure_battery': {
+		  	'command_class'				: 'COMMAND_CLASS_BATTERY',
+			  'command_get'				: 'BATTERY_GET',
+			  'command_report'			: 'BATTERY_REPORT',
+			  'command_report_parser'		: function( report ) {
+				  if( report['Battery Level'] === "battery low warning" ) return 1;
+				 return report['Battery Level (Raw)'][0];
+			      }
+		    },
 
-      'alarm_tamper': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_BINARY',
-			'command_get'				: 'SENSOR_BINARY_GET',
-			'command_report'			: 'SENSOR_BINARY_REPORT',
-			'command_report_parser'		: function( report ){
-				return report['Sensor Value'] === 'detected an event';
-			}
-		},
-	    
-      'measure_temperature': {
-      			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-      			'command_get'				: 'SENSOR_MULTILEVEL_GET',
-      			'command_get_parser'		: function(){
-      				return {
-      					'Sensor Type': 'Temperature (version 1)'
-      				}
-      			},
-      			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
-      			'command_report_parser'		: function( report ) {
+        'alarm_tamper': {
+        			'command_class'				: 'COMMAND_CLASS_NOTIFICATION',
+        			'command_get'				: 'NOTIFICATION_GET',
+        			'command_get_parser'		: function(){
+        				return {
+        					'V1 Alarm Type': 0,
+        					'Notification Type': 'Home Security',
+        					'Event': 3
+        				}
+        			},
+        			'command_report'			: 'NOTIFICATION_REPORT',
+        			'command_report_parser'		: function( report ){
 
-      				if( report['Sensor Type'] !== 'Temperature (version 1)' )
-      					return null;
+        				if( report['Notification Type'] !== 'Home Security' )
+        					return null;
 
-      				return report['Sensor Value (Parsed)'];
-      			}
-      		},
-      	'measure_battery': {
-      			'command_class'				: 'COMMAND_CLASS_BATTERY',
-      			'command_get'				: 'BATTERY_GET',
-      			'command_report'			: 'BATTERY_REPORT',
-      			'command_report_parser'		: function( report ) {
-      				if( report['Battery Level'] === "battery low warning" ) return 1;
-      				return report['Battery Level (Raw)'][0];
-      			}
-      		}
-      	},
+        				if( report['Event (Parsed)'] === 'Event inactive' ) {
+        					return false;
+        				} else if( report['Event (Parsed)'] === 'Tampering, Product covering removed' ) {
+        					return true;
+        				} else {
+        					return null
+        				}
+        			}
+        		},
+
+    	},
     settings: {
                 "siren_trigger_mode": {
                 "index": 1,
@@ -76,3 +75,36 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
               }
         }
 );
+
+
+Homey.manager('flow').on('action.sound_alarm', function( callback, args ){
+	Homey.log('');
+	Homey.log('on flow action.action.sound_alarm');
+	Homey.log('args', args);
+
+	Homey.manager('drivers').getDriver('schreeuwer').capabilities.onoff.set(args.device, true, function (err, data) {
+		Homey.log('');
+		Homey.log('Homey.manager(drivers).getDriver(schreeuwer).capabilities.onoff.set');
+		Homey.log('err', err);
+		Homey.log('data', data);
+		if (err) callback (err, false);
+	});
+
+	callback( null, true ); // we've fired successfully
+});
+
+Homey.manager('flow').on('action.silence_alarm', function( callback, args ){
+	Homey.log('');
+	Homey.log('on flow action.action.silence_alarm');
+	Homey.log('args', args);
+
+	Homey.manager('drivers').getDriver('schreeuwer').capabilities.onoff.set(args.device, false, function (err, data) {
+		Homey.log('');
+		Homey.log('Homey.manager(drivers).getDriver(schreeuwer).capabilities.onoff.set');
+		Homey.log('err', err);
+		Homey.log('data', data);
+		if (err) callback (err, false);
+	});
+
+	callback( null, true ); // we've fired successfully
+});
