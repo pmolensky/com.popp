7b45fefdf2c56de125f09fc552efd77680b25794
mruiter <mruiter@tuxweb.nl>
2016-08-16 08:30:13 +0200

    Sirene werkend in triggers
    Temperature en Battery nog

--- drivers/schreeuwer/driver.js
+++ drivers/schreeuwer/driver.js
@@ -31,30 +31,30 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		    },
 
         'alarm_tamper': {
-        			'command_class'				: 'COMMAND_CLASS_NOTIFICATION',
-        			'command_get'				: 'NOTIFICATION_GET',
-        			'command_get_parser'		: function(){
-        				return {
-        					'V1 Alarm Type': 0,
-        					'Notification Type': 'Home Security',
-        					'Event': 3
-        				}
-        			},
-        			'command_report'			: 'NOTIFICATION_REPORT',
-        			'command_report_parser'		: function( report ){
+			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
+			command_get: 'SENSOR_BINARY_GET',
+			command_report: 'SENSOR_BINARY_REPORT',
+			command_report_parser: report => report['Sensor Value'] === 'detected an event'
+		},
+		'measure_temperature': {
+			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get'				: 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser'		: function(){
+				return {
+					'Sensor Type': 'Temperature (version 1)',
+					'Properties1': {
+						'Scale': 0
+					}
+				}
+			},
+			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
+			'command_report_parser'		: function( report ){
+				if( report['Sensor Type'] !== 'Temperature (version 1)' )
+					return null;
 
-        				if( report['Notification Type'] !== 'Home Security' )
-        					return null;
-
-        				if( report['Event (Parsed)'] === 'Event inactive' ) {
-        					return false;
-        				} else if( report['Event (Parsed)'] === 'Tampering, Product covering removed' ) {
-        					return true;
-        				} else {
-        					return null
-        				}
-        			}
-        		},
+				return report['Sensor Value (Parsed)'];
+			}
+		},
 
     	},
     settings: {
