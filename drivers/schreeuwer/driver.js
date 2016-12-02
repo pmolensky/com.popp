2bbc5088afe66e430affec803a3e1de9cd7d5da1
mruiter <mruiter@tuxweb.nl>
2016-12-02 13:34:44 +0100

    Complete cleanup / zwave driver rewrites on sensors
    Nice clean and ready for release

--- drivers/schreeuwer/driver.js
+++ drivers/schreeuwer/driver.js
@@ -9,68 +9,73 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
     debug: true,
     capabilities: {
     		'onoff': {
-    			'command_class'			: 'COMMAND_CLASS_SWITCH_BINARY',
-    			'command_set'				: 'SWITCH_BINARY_SET',
-    			'command_set_parser'		: function( value ){
+	            'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
+	            'command_get': 'SWITCH_BINARY_GET',
+	            'command_set': 'SWITCH_BINARY_SET',
+              'command_set_parser': value => {
+		              return {
+			                 'Switch Value': (value > 0) ? 'on/enable' : 'off/disable'
+		                   };
+              },
+	            'command_report': 'SWITCH_BINARY_REPORT',
+            	'command_report_parser': report => report['Value'] === 'on/enable'
+        },
+
+
+        'alarm_tamper': {
+    			'command_class'			: 'COMMAND_CLASS_SENSOR_BINARY',
+    			'command_get'				: 'SENSOR_BINARY_GET',
+    			'command_get_parser'		: function(){
     				return {
-    					'Switch Value': value
+    					'Sensor Type': 'General Purpose Alarm'
     				}
     			},
-    			'command_report'			: 'SWITCH_BINARY_REPORT',
+    			'command_report'			: 'SENSOR_BINARY_REPORT',
     			'command_report_parser'		: function( report ){
-    				return report['Value'] === 'on/enable';
+    				Homey.log('[EVR DEBUG] alarm_tamper report:', report);
+    				return report['Sensor State'] === 'alarm';
     			}
     		},
-			
-			'measure_temperature': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			'command_get'				: 'SENSOR_MULTILEVEL_GET',
-			'command_get_parser'		: function(){
-				return {
-					'Sensor Type': 'Temperature (version 1)',
-					'Properties1': {
-						'Scale': 0
-					}
-				}
-			},
-			'command_report'			: 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser'		: function( report ){
-				if( report['Sensor Type'] !== 'Temperature (version 1)' )
-					return null;
-
-				return report['Sensor Value (Parsed)'];
-			}
-		},
 
 
-		'alarm_tamper': {
-			'command_class'				: 'COMMAND_CLASS_SENSOR_BINARY',
-			'command_get'				: 'SENSOR_BINARY_GET',
-			'command_get_parser'		: function(){
-				return {
-					'Sensor Type': 'General Purpose Alarm'
-				}
-			},
-			'command_report'			: 'SENSOR_BINARY_REPORT',
-			'command_report_parser'		: function( report ){
-				Homey.log('[EVR DEBUG] alarm_tamper report:', report);
-				return report['Sensor State'] === 'alarm';
-			}
-		},
 
-		'measure_battery': {
-			'command_class'				: 'COMMAND_CLASS_BATTERY',
-			'command_get'				: 'BATTERY_GET',
-			'command_report'			: 'BATTERY_REPORT',
-			'command_report_parser'		: function( report ) {
-				Homey.log('[EVR DEBUG] measure_battery report:', report);
-				if( report['Battery Level'] === "battery low warning" ) return 1;
-				return report['Battery Level (Raw)'][0];
-			}
-		}
+        'measure_temperature': {
+  	       'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+  	       'command_get': 'SENSOR_MULTILEVEL_GET',
+  	       'command_get_parser': () => {
+  		           return {
+  			   'Sensor Type': 'Temperature (version 1)',
+  			   'Properties1': {
+  				  'Scale': 0
+  			}
+  		};
+  	},
+  	'command_report': 'SENSOR_MULTILEVEL_REPORT',
+  	'command_report_parser': report => {
+  		if (report['Sensor Type'] === "Temperature (version 1)" &&
+  		report.hasOwnProperty("Level") &&
+  		report.Level.hasOwnProperty("Scale") &&
+  		report.Level.Scale === 0)
+  			return report['Sensor Value (Parsed)'];
 
-    	},
-    settings: {
+  		return null;
+  	}
+  },
+
+
+    'measure_battery': {
+	       'command_class': 'COMMAND_CLASS_BATTERY',
+	       'command_get': 'BATTERY_GET',
+	       'command_report': 'BATTERY_REPORT',
+	           'command_report_parser': report => {
+		             if (report['Battery Level'] === "battery low warning")
+			              return 1;
+				        return report['Battery Level (Raw)'][0];
+	             }
+    }
+	},
+
+  settings: {
                 "siren_trigger_mode": {
                 "index": 1,
                 "size": 1,
