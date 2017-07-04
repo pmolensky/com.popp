8cd2d5209aef6885dfc9e522c79a73148f586848
Marco Ruiter <mruiter@tuxweb.nl>
2017-07-04 15:10:21 +0200

    Added Meter Class to wall switches

--- drivers/009006/driver.js
+++ drivers/009006/driver.js
@@ -16,50 +16,114 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					}),
 					'command_report': 'SWITCH_BINARY_REPORT',
 					'command_report_parser': report => report.Value === 'on/enable'
-				}		
-		},
+				},
+			measure_power: {
+					command_class: 'COMMAND_CLASS_METER',
+					command_get: 'METER_GET',
+					command_get_parser: () => ({
+						Properties1: {
+							'Rate Type': 'Import',
+							Scale: 2,
+						},
+						'Scale 2': 0,
+					}),
+			command_report: 'METER_REPORT',
+					command_report_parser: report => {
+						if (report.hasOwnProperty('Properties2') &&
+							report.Properties2.hasOwnProperty('Scale bits 10') &&
+							report.Properties2['Scale bits 10'] === 2) {
+							return report['Meter Value (Parsed)'];
+						}
+						return null;
+					},
+				},
+			meter_power: {
+					command_class: 'COMMAND_CLASS_METER',
+					command_get: 'METER_GET',
+					command_get_parser: () => ({
+						Properties1: {
+							'Rate Type': 'Import',
+							Scale: 0,
+						},
+						'Scale 2': 0,
+					}),
+					command_report: 'METER_REPORT',
+					command_report_parser: report => {
+						if (report.hasOwnProperty('Properties2') &&
+							report.Properties2.hasOwnProperty('Size') &&
+							report.Properties2.Size === 4 &&
+							report.Properties2.hasOwnProperty('Scale bits 10') &&
+							report.Properties2['Scale bits 10'] === 0) {
+							return report['Meter Value (Parsed)'];
+						}
+						return null;
+					},
+				},
+			measure_voltage: {
+					command_class: 'COMMAND_CLASS_METER',
+					command_get: 'METER_GET',
+					command_get_parser: () => ({
+						Properties1: {
+							'Rate Type': 'Import',
+							Scale: 2,
+						},
+						'Scale 2': 0,
+					}),
+					command_report: 'METER_REPORT',
+					command_report_parser: report => {
+						if (report.hasOwnProperty('Properties2') &&
+							report.Properties2.hasOwnProperty('Size') &&
+							report.Properties2.Size === 2 &&
+							report.Properties2.hasOwnProperty('Scale bits 10') &&
+							report.Properties2['Scale bits 10'] === 0) {
+							return report['Meter Value (Parsed)'];
+						}
+						return null;
+					},
+				},
+			measure_current: {
+					command_class: 'COMMAND_CLASS_METER',
+					command_get: 'METER_GET',
+					command_get_parser: () => ({
+						Properties1: {
+							'Rate Type': 'Import',
+							Scale: 2,
+						},
+						'Scale 2': 0,
+					}),
+					command_report: 'METER_REPORT',
+					command_report_parser: report => {
+						if (report.hasOwnProperty('Properties2') &&
+							report.Properties2.hasOwnProperty('Size') &&
+							report.Properties2.Size === 2 &&
+							report.Properties2.hasOwnProperty('Scale bits 10') &&
+							report.Properties2['Scale bits 10'] === 1) {
+							return report['Meter Value (Parsed)'];
+						}
+						return null;
+					},
+				}	
+			},
 		settings : {
 		"led_indication_mode": {
 			"index": 1,
 			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
 		},
 		"auto_switch_off": {
 			"index": 2,
 			"size": 2,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
 		},
 		"state_after_powerloss": {
 			"index": 5,
 			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
 		},
-		"energy_consumption": {
-			"index": 20,
-			"size": 2,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
-		},
-			"led_off_color": {
+		"led_off_color": {
 			"index": 21,
 			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
 		},
-			"led_on_color": {
+		"led_on_color": {
 			"index": 22,
 			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
 		}
 	}
 	})
