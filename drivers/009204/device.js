941d9e628093cdd7d83160a8e463baa0c8531a96
Marco Ruiter <mruiter@tuxweb.nl>
2017-12-10 08:44:59 +0100

    KeyFob Added

---
+++ drivers/009204/device.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Homey = require('homey');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class P009204 extends ZwaveDevice {
+  async onMeshInit() {
+    let PreviousSequenceNo = 'empty';
+
+    //this.enableDebug();
+    //this.printNode();
+    this.registerCapability('alarm_battery', 'BATTERY');
+    this.registerCapability('measure_battery', 'BATTERY', {
+      getOpts: {
+        getOnOnline: true,
+      }
+    });
+
+
+    // define and register FlowCardTriggers
+    let triggerRC_scene = new Homey.FlowCardTriggerDevice('RC_scene');
+    triggerRC_scene
+      .register()
+      .registerRunListener((args, state) => {
+        //this.log(args, state);
+        return Promise.resolve(args.button === state.button && args.scene === state.scene);
+      });
+
+
+    // register a report listener (SDK2 style not yet operational)
+    this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (rawReport, parsedReport) => {
+      if (rawReport.hasOwnProperty('Properties1') &&
+        rawReport.Properties1.hasOwnProperty('Key Attributes') &&
+        rawReport.hasOwnProperty('Scene Number') &&
+        rawReport.hasOwnProperty('Sequence Number')) {
+        if (rawReport['Sequence Number'] !== PreviousSequenceNo) {
+          const remoteValue = {
+            button: rawReport['Scene Number'].toString(),
+            scene: rawReport.Properties1['Key Attributes'],
+          };
+          PreviousSequenceNo = rawReport['Sequence Number'];
+          this.log('Triggering sequence:', PreviousSequenceNo, 'remoteValue', remoteValue);
+          // Trigger the trigger card with 2 dropdown options
+          triggerRC_scene.trigger(this, triggerRC_scene.getArgumentValues, remoteValue);
+        }
+      }
+    });
+
+
+
+  }
+}
+module.exports = P009204;
