0902c0774204340fa55303437cd67da835366390
Marco Ruiter <mruiter@tuxweb.nl>
2018-02-20 11:20:54 +0100

    z-wave Weather

--- drivers/005206/device.js
+++ drivers/005206/device.js
@@ -7,21 +7,27 @@ class P005206 extends ZwaveDevice {
   onMeshInit() {
       // this.enableDebug();
       // this.printNode();
-      this.registerCapability('measure_wind_strenght', 'SENSOR_MULTILEVEL', {
-            get: 'SENSOR_MULTILEVEL_GET',
-            getParser: () => ({
-              'Sensor Type': 'Velocity (version 2)',
-              Properties1: {
-                Scale: 0,
-              },
-            }),
-            report: 'SENSOR_MULTILEVEL_REPORT',
-            reportParser: report => {
-              if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-                if (report['Sensor Type'] === 'Velocity (version 2)') return report['Sensor Value (Parsed)'];
-              }
-              return null;
-            }
+      this.registerCapability('measure_wind_strenght', 'METER_WIND', {
+				get: 'METER_GET',
+				getParser: () => ({
+					return {
+						'Properties1': {
+						'Rate Type': '2'
+						},
+					}
+				}),
+				report: 'METER_REPORT',
+				reportParser: report => {
+					if (report.hasOwnProperty('Properties1')
+						&& report.Properties1.hasOwnProperty('Meter Type')
+						&& report.Properties1['Meter Type'] === "1"
+						&& report.hasOwnProperty('Properties2')
+						&& report.Properties2.hasOwnProperty('Scale bits 10')
+						&& report.Properties2['Scale bits 10'] === 0) {
+							return report['Meter Value (Parsed)'];
+						}
+					return null;
+				}
           });
       this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
       this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
@@ -29,7 +35,7 @@ class P005206 extends ZwaveDevice {
       this.registerCapability('measure_pressure', 'SENSOR_MULTILEVEL', {
               get: 'SENSOR_MULTILEVEL_GET',
               getParser: () => ({
-                'Sensor Type': 'Atmospheric pressure (version 2)',
+                'Sensor Type': 'Barometric pressure (version 2)',
                 Properties1: {
                   Scale: 0,
                 },
@@ -37,7 +43,7 @@ class P005206 extends ZwaveDevice {
               report: 'SENSOR_MULTILEVEL_REPORT',
               reportParser: report => {
                 if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-                  if (report['Sensor Type'] === 'Atmospheric pressure (version 2)') return report['Sensor Value (Parsed)'];
+                  if (report['Sensor Type'] === 'Barometric pressure (version 2)') return report['Sensor Value (Parsed)'];
                 }
                 return null;
               }
