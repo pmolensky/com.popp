6fcd2cdb5bd43740801308b2ddff81c26be8be06
Paul Molensky <pmolensky@users.noreply.github.com>
2018-12-31 00:36:47 +0100

    Revert "[700045] Extended keypad capabilities"
    This reverts commit d1d3934676270c45eeec2c5616669da88d70547f.

--- drivers/700045/device.js
+++ drivers/700045/device.js
@@ -11,27 +11,13 @@ class P700045 extends ZwaveDevice {
     this.registerCapability('alarm_battery', 'BATTERY');
 
     // define and register FlowCardTriggers
-    let triggerUser = new Homey.FlowCardTriggerDevice('user_validated');
-    triggerUser
+    let triggerRC_scene = new Homey.FlowCardTriggerDevice('RC_scene');
+    triggerRC_scene
       .register()
       .registerRunListener((args, state) => {
         //this.log(args, state);
-        return Promise.resolve(args.button === state.button || (args.button == 0 && state.button >= 1 && state.button <= 20) /* && args.scene === state.scene */ );
-      })
-
-    let triggerRing = new Homey.FlowCardTriggerDevice('ring_button');
-    triggerRing
-      .register()
-      .registerRunListener((args, state) => {
-        return Promise.resolve(args.button === state.button );
-      })
-
-    let triggerInvalid = new Homey.FlowCardTriggerDevice('code_invalid');
-    triggerInvalid
-      .register()
-      .registerRunListener((args, state) => {
-        return Promise.resolve(state.button === '23' );
-      })
+        return Promise.resolve(args.button === state.button && args.scene === state.scene);
+      });
 
     // register a report listener (SDK2 style not yet operational)
     this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (rawReport, parsedReport) => {
@@ -48,19 +34,7 @@ class P700045 extends ZwaveDevice {
           PreviousSequenceNo = rawReport['Sequence Number'];
           this.log('Triggering sequence:', PreviousSequenceNo, 'remoteValue', remoteValue);
           // Trigger the trigger card with 2 dropdown options
-          if(remoteValue.button == 21 || remoteValue.button == 22) { // bell-button: 21=once; 22=twice
-            triggerRing
-              .trigger(this, triggerRing.getArgumentValues, remoteValue)
-              .catch( err => { this.log('ReportListener', err) } );
-          } else if(remoteValue.button == 23) { // unrecognized user code
-            triggerInvalid
-              .trigger(this, triggerInvalid.getArgumentValues, remoteValue)
-              .catch( err => { this.log('ReportListener', err) } );
-          } else { //assuming user code 1-20
-            triggerUser
-              .trigger(this, triggerUser.getArgumentValues, remoteValue)
-              .catch( err => { this.log('ReportListener', err) } );
-          }
+          triggerRC_scene.trigger(this, triggerRC_scene.getArgumentValues, remoteValue);
         }
       }
     });
